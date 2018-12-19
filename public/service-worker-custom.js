d29622d0b79fbbd7b79d9a2a4027de4bc28a2411
Stephannie Hilario <hilariostephannie@gmail.com>
2018-12-19 11:39:37 -0400

    Add service worker

---
+++ public/service-worker-custom.js
@@ -0,0 +1,61 @@
+this.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open('neighborhood-cache').then(function (cache) {
+      return cache.addAll([
+        '/',
+        '../utils/Places.js',
+        '../src/icons/search.svg',
+        '../src/App.js',
+        '../src/Map.js',
+        '../src/SideNavBar.js',
+        '../src/App.css',
+        '../src/Map.css'
+      ]);
+    }).catch(err => console.log('ERROR', err))
+  );
+});
+
+// Event that when fired deletes the cache
+this.addEventListener('activate', function (event) {
+  let cacheName = ['neighborhood-cache'];
+  event.waitUntil(
+    caches.keys().then(function (keyList) {
+      return Promise.all(keyList.map(function (key) {
+        if(cacheName.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      }));
+    }).catch(err => console.log('ERROR', err))
+  );
+});
+
+// Event that intercept any resource controlled by the service worker
+this.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) {
+        return response;
+      }
+
+      // Clone the request
+      let cloneRequest = event.request.clone();
+
+      return fetch(cloneRequest).then(
+        function (response) {
+          // Checks if the received response is a valid one
+          if (!response || response.status != 200 || response.type !== 'basic') {
+            return response;
+          }
+
+          let responseToCache = response.clone();
+
+          caches.open('neighborhood-cache').then(function (cache) {
+            cache.put(event.request, responseToCache);
+          });
+
+          return response;
+        }
+      )
+    })
+  );
+});
